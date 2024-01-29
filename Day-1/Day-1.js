3ca9675932aeb5b02ae57602a7dcb5ed04c6d0bd
atharvamene025 <atharvamene@gmail.com>
2024-01-29 20:59:44 +0530

    DAY 1 updates

---
+++ Day-1/Day-1.js
@@ -0,0 +1,106 @@
+// datatypes :
+
+// information :
+// name :
+// age :
+// number
+// yes / no boleamn data types
+
+//String should be written in double inverted commas,single invetred commas or back tick
+// {"",'',``}
+
+console.log("Atharva")
+console.log(typeof("Test@123"))
+console.log('Using Single inverted Commas')
+console.log(typeof('true'))
+console.log(('Using back tick'))
+console.log(typeof(`Back tick`))
+
+console.log("one","two","three") 
+// testing multiple values in one
+
+console.log(2+2)
+
+Number:
+
+console.log(1234,typeof(1234))
+console.log("1234",typeof(1234))
+
+console.log(1+1)
+// Numbers should be wirtten without inverted commas
+console.log("1"+"1")
+// If written in inverted commas willbe treated as 
+
+// Boolean
+// true,false
+
+console.log(true, typeof(true))
+console.log(false, typeof(false))
+console.log('false', typeof(false))
+
+//Variables
+//data store
+
+//var
+// let
+// const
+
+var collegename = "MIT"
+var age = 21
+var isOpen = true
+console.log(collegename,age,isOpen)
+
+var value1=10;
+var value2=20;
+
+console.log(value1+value2)
+console.log(value1-value2)
+console.log(value1/value2)
+console.log(value1*value2)
+
+var anything =24
+console.log(anything)
+console.log(anything)
+console.log(anything)
+console.log(anything)
+
+var myValue=10;
+console.log(myValue)
+myValue=20; //reassingned value
+console.log(myValue)
+myValue = 50;//re-declare
+console.log(myValue)
+
+// const kuchbhi =2345;
+// kuchbhi=25;
+// const kuchbhi=3456;
+// console.log(kuchbhi)
+// you cannot re assign or re decalre
+
+
+// let kuchbhi=1234;
+// kuchbhi =23456;
+// let kuchbhi=3456;
+// console.log(kuchbhi)
+
+
+//variable functions- Function is Scoped,hoisting is possible
+//reassign re declare possible
+//Const functions- Function is Blocked,hoisting is not possible
+//re-assign re-declare not possible
+//let functions- Function is Scoped,hoisting is not possible
+//re-assign re-declare notpossible
+
+//joisting is js behaviour 
+
+// myname = 'Atharva'; // assigning
+
+// console.log(myname) // log
+
+// var myname; // declare
+
+// myname = 'awdiz';
+
+// console.log(myname)
+
+// const , let myname ;
