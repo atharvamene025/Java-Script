06ba79ca8d9026b5a4961b5abd0f6e028b05c0c9
atharvamene025 <atharvamene@gmail.com>
2024-02-02 14:02:54 +0530

    updates Day 3

---
+++ Day-3/Day-3.js
@@ -0,0 +1,34 @@
+// if(1>3){
+//     console.log("Condition is ture")
+// } else
+// {
+//     console.log("condtion is false")
+// }
+
+// var value1=11;
+// var value2=15;
+// var value3=20;
+
+// if(value1>value2){
+//     console.log(
+//     "val1 is bigger than val 2"
+//     )
+// } else if (value1>value3){
+//     console.log("val 1 is bigger than val 2 and 3")
+// }else (
+//     console.log("value3 is bigger")
+// )
+
+
+var age = 82;
+var minage = 18;
+var maxage = 80;
+
+if (age>minage && age<maxage)
+{
+    console.log("You are Eligible for Liscence")
+}else if (age==18) {
+    console.log("You are Eligible for Learning liscense")
+}else {
+    console.log("You are NOT Eligible for  Liscence")
+}
---
+++ Day-3/tempCodeRunnerFile.js
@@ -0,0 +1,6 @@
+if(1>3){
+//     console.log("Condition is ture")
+// } else
+// {
+//     console.log("condtion is false")
+// }
