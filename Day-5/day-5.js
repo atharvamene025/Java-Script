d24f7481645f2c8b5cc63e5a2a3b9bff43fc4854
atharvamene025 <atharvamene@gmail.com>
2024-02-05 13:29:01 +0530

    updates

---
+++ Day-5/day-5.js
@@ -0,0 +1,17 @@
+// function atharva(){
+//     console.log("Hello Everyone" )
+// }
+// function mene(){
+//     console.log("Hello Everyone2" )
+// }
+
+
+
+function addition{
+    var count=0;
+    for (var i=value;i<=value2;i++){
+        if(1%2==1){
+            count+=i
+        }
+    }
+}
