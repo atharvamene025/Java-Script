c5ad1c4727961f805de04e7b89d6163904f2b5fc
atharvamene025 <atharvamene@gmail.com>
2024-02-09 22:48:59 +0530

    Update day-5.js

--- Day-5/day-5.js
+++ Day-5/day-5.js
@@ -6,12 +6,68 @@
 // }
 
 
+//Q. Even and Odd No between 1 to 20
+// for(i=0;i<=20;i++){
+//     if(i%2==0){
+//         console.log(i,"Even")
+//     }else{
+//         console.log(i,"Odd")
+//     }
+// }
 
-function addition{
-    var count=0;
-    for (var i=value;i<=value2;i++){
-        if(1%2==1){
-            count+=i
-        }
+//Q. Print addition Of Even No From 10 to 30
+// var count=0;
+// for(i=10;i<=30;i++){
+//     if(i%2==0){
+//         count+=i
+//     }
+
+// }
+// console.log(count)
+
+// //Q. Print substraction of Odd No from 100 to 10
+// var sub=10;
+// for(i=10;i>=1;i--){
+//     if(i%2==1){
+//         sub-=i
+//     }
+// }
+// console.log(sub)
+
+
+// function addition(value1,value2){
+//     console.log(value1+value2)
+// }
+
+// addition(10,10)
+
+// Q. Find Additon of odd no from given range
+
+
+// function addition(value1, value2) {
+
+//     var count = 0;
+//     for (i = value1; i <= value2; i++) {
+//         if (i % 2 == 1) {
+//             count += i
+//         }
+//     } return count;
+
+// }
+
+// const answer = addition(10, 13);
+// console.log(answer)
+
+
+// Q To Find The Factorial Of given Node
+
+function fact(){
+
+    var ans=1
+    for(var i=value;i>=value;i--){
+        ans*=i
     }
+    return ans
 }
+
+fact(10)
