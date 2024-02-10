c8c38b59fe956a3f095ae1ac08af4d1df8b6868a
atharvamene025 <atharvamene@gmail.com>
2024-02-10 11:27:26 +0530

    Updates

--- Day-7/Day-7.js
+++ Day-7/Day-7.js
@@ -1,13 +1,13 @@
-var name= "abcdacda"
+var name = "abcdacda"
 
 
-function isStringPalindrome(string) {
-    for(var i=0;i< Math.floor(string.length/2);i++){
-        console,log(string[i], string[string.length-1-i])
-        if (string[i] !== string[string.length-1-i]) {
+function isstringpalindrome(string) {
+    for (var i = 0; i < Math.floor(string.length / 2); i++) {
+        console.log(string[i], string[string.length - 1 - i])
+        if (string[i] !== string[string.length - 1 - i]) {
             return "Not Palindrome"
         }
-    } 
-     return "It Is A Palindrome"   
+     }
+      "It Is A Palindrome"
 }
-console.log(isStringPalindrome(name))
+console.log(isstringpalindrome(name))
