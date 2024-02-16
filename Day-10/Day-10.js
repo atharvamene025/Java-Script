73e46a9a604b59e44e36183bde46c977aa7d6eca
atharvamene025 <atharvamene@gmail.com>
2024-02-17 00:06:43 +0530

    updates

---
+++ Day-10/Day-10.js
@@ -0,0 +1,26 @@
+
+// concat
+
+var Cars=["BMW", "Audi","Volvo"]
+var Bikes=["Yamaha","KTM","Java"]
+
+const newArray = Cars.concat(Bikes);
+console.log(newArray)
+
+
+var array =[1,2,3]
+const isExist = array.includes(2)
+console.log(isExist)
+
+var Students = ["Atharva","Tanmay","Aditya","Vedant",]
+console.log(Students.sort())
+
+var Rollno = [25,24,76,34,89,54,21,12]
+console.log(Rollno.sort())
+
+var info= ["ijk","lmn","pqr"]
+
+console.log(info.join("-"))
+console.log(info.join(" "))
+console.log(info.join(""))
+console.log(info.join())
